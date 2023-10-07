e188f7c20991e1a47bdcb6bd13b5931937a16d19
artemdadi <146953527+artemdadi@users.noreply.github.com>
2023-10-07 15:05:26 +0300

    Update build and run.bat

--- build and run.bat
+++ build and run.bat
@@ -1,6 +1,20 @@
 @echo off
+
 set /p lesson_n="Write lesson number: "
 set /p task_n="Write task number: "
+
 javac -d %CD% %lesson_n%\%task_n%.java
+
+:run
 java program
+
+set /p again="Run again?[y/n] "
+if %again% equ y (
+	call :run
+) else (
+	call :end
+)
+
+:end
 pause
+exit
